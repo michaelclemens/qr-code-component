c2ee4d1e9d2508d8ad9cd1041645a9b82e9707a0
michaelclemens <michaelclemens8@hotmail.com>
2025-02-12 14:32:05 +0000

    Adding QR component

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,17 @@
-import { useState } from 'react'
+import qrCodeImg from './assets/image-qr-code.png'
+import Card from './components/Card'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount(count => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-    </>
+    <main className="font-outfit flex h-screen flex-col items-center justify-center bg-slate-300">
+      <Card>
+        <img src={qrCodeImg} alt="QR code image" className="mb-6 rounded-[10px]" />
+        <div className="space-y-4 px-4 text-center">
+          <h1 className="text-header text-slate-900">Improve your front-end skills by building projects</h1>
+          <p className="text-paragraph text-slate-500">Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
+        </div>
+      </Card>
+    </main>
   )
 }
 
---
+++ src/components/Card.tsx
@@ -0,0 +1,5 @@
+const Card = ({ children }: { children: React.ReactNode }) => (
+  <div className="drop-shadow-card w-80 rounded-[20px] bg-white px-4 pt-4 pb-10">{children}</div>
+)
+
+export default Card
